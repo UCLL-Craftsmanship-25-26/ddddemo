545b456a79cef9661e4251b25e96dc22fd9321ad
Jeroen Rombouts <jeroen.rombouts@ucll.be>
2025-10-16 02:12:30 +0200

    book jpa

--- src/main/java/be/ucll/craftmanship/DDDDemo/library/catalog/domain/Book.java
+++ src/main/java/be/ucll/craftmanship/DDDDemo/library/catalog/domain/Book.java
@@ -1,7 +1,19 @@
 package be.ucll.craftmanship.DDDDemo.library.catalog.domain;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Book {
+
+    @EmbeddedId
     private BookId id;
     private String title;
+    @Embedded
+    @AttributeOverride(name = "value", column = @Column(name = "isbn"))
     private Isbn isbn;
+
+    protected Book(){
+        //for jpa
+    }
+
 }
